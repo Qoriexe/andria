c77f9a5788797cb48ba91e1957fe2105bf937316
Qoriexe <149482328+Qoriexe@users.noreply.github.com>
2025-12-19 00:47:03 +0400

    Add files via upload

---
+++ qori/script.js
@@ -0,0 +1,8 @@
+document.getElementById("burger-menu").addEventListener("click", () => {
+    document.getElementById("burger-nav").style.width = "100vh";
+});
+
+
+document.getElementById("close-btn").addEventListener("click", () => {
+    document.getElementById("burger-nav").style.width = "0px"
+} )
