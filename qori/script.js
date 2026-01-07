1cf0c5492b32fef9999fcd87bd1504681519aaad
Qoriexe <149482328+Qoriexe@users.noreply.github.com>
2026-01-07 22:58:35 +0400

    Delete qori directory

--- qori/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("burger-menu").addEventListener("click", () => {
-    document.getElementById("burger-nav").style.width = "100vh";
-});
-
-
-document.getElementById("close-btn").addEventListener("click", () => {
-    document.getElementById("burger-nav").style.width = "0px"
-} )
